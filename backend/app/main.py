9617a21d6aa8aa0d4abb9e844585e3e7ea8e1db6
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2026-02-16 01:55:43 +0530

    services setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploaded_images"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/upload-image")
+async def upload_image(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
