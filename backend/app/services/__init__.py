dd6b7b0fe23374b0d6ecfb63e3aa454230fc1c48
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2026-02-21 10:30:11 +0530

    fixed pipeline

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,6 +1,6 @@
-from geo_loc import Loc_api
-from weather_data import Weather_Data
-from cust_llm import Llm
-from weather_data import Vision_model
+from .geo_loc import Loc_api
+from .weather_data import Weather_Data
+from .cust_llm import Llm
+from .visoin_model import Vision_model
 
 __all__ = ['Loc_api','Llm','Vision_model','Weather_Data']
