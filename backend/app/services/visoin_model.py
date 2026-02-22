f186b89c835a811965760cdb4ca3adc2a5ae5dab
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2026-02-22 21:08:56 +0530

    done

--- backend/app/services/visoin_model.py
+++ backend/app/services/visoin_model.py
@@ -2,13 +2,19 @@ import os
 from dotenv import load_dotenv
 from ultralytics import YOLO
 import numpy as np
+from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parents[3]  
 
 class Vision_model():
     def __init__(self):
         'An image classification YOLO model'
+        # env_path = BASE_DIR / ".env"
         load_dotenv()
         model_path = os.getenv('video_model_path')
         self.image_store = os.getenv('image_store_path')
+        print(model_path)
+        # model_path = r'C:\Users\ASUS\OneDrive\Desktop\private\backend\app\model\best (1).pt'
         self.model = YOLO(model_path)
 
     def callasify(self,image) -> str:
@@ -18,6 +24,8 @@ class Vision_model():
         '''
         res = self.model(image)
         image = res[0]
+        if os.path.exists(self.image_store):
+            os.remove(self.image_store)
         image.save(filename=self.image_store)
         detected_classes = []
         for box in image.boxes:
